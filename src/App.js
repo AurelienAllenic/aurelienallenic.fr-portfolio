45d0d53b2965ad049f2e5fd5cae6c683cc4566e5
Aurelien Allenic <aurelien.allenic@gmail.com>
2023-01-06 15:30:32 +0100

    (feat) added a loader

--- src/App.js
+++ src/App.js
@@ -8,20 +8,46 @@ import Portfolio from './components/portfolio/Portfolio';
 import Interests from './components/interests/Interests';
 import Contact from './components/contact/Contact';
 import Navbar from './components/nav/Nav';
+import BarLoader from "react-spinners/BarLoader";
+import {useState, useEffect} from "react"
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    }, 3500)
+  }, [])
+
   return (
     <div className="App">
-
-        <Home/>
-        <Parallax  bgImage={test} strength={500}>
-          <Navbar/>
-        <About/>
-        <Experience/>
-        <Portfolio/>
-        <Interests/>
-        </Parallax>
-        <Contact/>
+        {
+        loading ?
+        <div className="loader">
+        <BarLoader
+        loading={loading}
+        size={800}
+        width={200}
+        color="#008ab3"
+        aria-label="Loading Spinner"
+        data-testid="loader"
+        speedMultiplier={1}
+      /></div>
+      :<>
+      <div className='animation-apparition'>
+          <Home/>
+          <Parallax  bgImage={test} strength={500}>
+            <Navbar/>
+            <About/>
+            <Experience/>
+            <Portfolio/>
+            <Interests/>
+          </Parallax>
+          <Contact/>
+        </div>
+        </>
+}
     </div>
   );
 }
